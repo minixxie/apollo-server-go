3311035fec459d122ced75742dc16550fdb85ff9
Brian So <brian.so@lalamove.com>
2022-05-04 15:43:34 +0800

    add: read configs from .properties file

--- go.mod
+++ go.mod
@@ -4,5 +4,8 @@ go 1.14
 
 require (
 	github.com/gin-gonic/gin v1.5.0
-	github.com/spf13/viper v1.6.2
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/magiconair/properties v1.8.6 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/yaml.v2 v2.2.4 // indirect
 )
--- main.go
+++ main.go
@@ -1,25 +1,29 @@
 package main
 
 import (
-	"os"
-	"log"
-	"fmt"
-	"time"
-	"strconv"
-	"io/ioutil"
 	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/magiconair/properties"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func main() {
-	file, err := ioutil.ReadFile("/config.json")
-	if err != nil {
-		log.Fatalf("Please prepare /config.json")
+	var configMap map[string]interface{}
+	var err error
+	if os.Getenv("CONFIG_FORMAT") == "properties" {
+		configMap, err = loadConfigProperties()
+	} else {
+		configMap, err = loadConfigJson()
 	}
-	configMap := make(map[string]interface{})
-	err = json.Unmarshal([]byte(file), &configMap)
+
 	if err != nil {
-		log.Fatalf("ERR reading /config.json")
+		return
 	}
 
 	r := gin.Default()
@@ -28,6 +32,89 @@ func main() {
 	r.GET("/configfiles/json/:appId/:cluster/:namespace", queryConfigJSON(configMap))
 	r.GET("/notifications/v2", notificationsLongPolling(configMap))
 	r.Run(":80")
+}
+
+func loadConfigProperties() (map[string]interface{}, error) {
+	configMap := map[string]interface{}{}
+
+	// Read config folder
+	appIdFolders, err := ioutil.ReadDir("/configs")
+	if err != nil {
+		log.Printf("Cannot read config folder")
+		return nil, err
+	}
+	for _, appIdFolder := range appIdFolders {
+		if !appIdFolder.IsDir() {
+			continue
+		}
+		appId := appIdFolder.Name()
+		appConfig := map[string]interface{}{}
+		configMap[appId] = appConfig
+
+		// Read appId folder
+		clusterFolders, err := ioutil.ReadDir("/configs/" + appId)
+		if err != nil {
+			log.Printf("Cannot read cluster folder for appId=" + appId)
+			return nil, err
+		}
+
+		for _, clusterFolder := range clusterFolders {
+			if !clusterFolder.IsDir() {
+				continue
+			}
+			clusterName := clusterFolder.Name()
+			clusterConfig := map[string]interface{}{}
+			appConfig[clusterName] = clusterConfig
+
+			// Read Cluster folder
+			namespaceFiles, err := ioutil.ReadDir("/configs/" + appId + "/" + clusterName)
+			if err != nil {
+				log.Printf("Cannot read cluster folder for appId=" + appId + ", clusterName=" + clusterName)
+				return nil, err
+			}
+
+			for _, namespaceFile := range namespaceFiles {
+				if !strings.HasSuffix(namespaceFile.Name(), ".properties") {
+					continue
+				}
+				namespace := strings.TrimSuffix(namespaceFile.Name(), ".properties")
+				p, err := properties.LoadFile("/configs/" + appId + "/" + clusterName + "/" + namespaceFile.Name(), properties.UTF8)
+				if err != nil {
+					log.Printf("Cannot read namespace file: " + namespaceFile.Name())
+					return nil, err
+				}
+
+				// convert properties to map[string]interface{}
+				configurations := map[string]interface{}{}
+				for key, value := range p.Map() {
+					configurations[key] = value
+				}
+
+				// add namespace config to the config
+				clusterConfig[namespace] = map[string]interface{}{
+					"releaseKey": namespaceFile.ModTime().Format("20060102150405") + "-7fec91b6d277b5ab",
+					"configurations": configurations,
+				}
+			}
+		}
+	}
+
+	return configMap, nil
+}
+
+func loadConfigJson() (map[string]interface{}, error) {
+	file, err := ioutil.ReadFile("/config.json")
+	if err != nil {
+		log.Printf("Please prepare /config.json")
+		return nil, err
+	}
+	configMap := make(map[string]interface{})
+	err = json.Unmarshal([]byte(file), &configMap)
+	if err != nil {
+		log.Printf("ERR reading /config.json")
+		return nil, err
+	}
+	return configMap, nil
 }
 
 func queryConfigValidation(c *gin.Context, configMap map[string]interface{}) (map[string]interface{}, string) {
